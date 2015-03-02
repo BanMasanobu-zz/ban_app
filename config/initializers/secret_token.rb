1a50817eeabbaced3212f3bb2a41a872c9ded497
BanMasanobu <babababandits@gmail.com>
2015-03-02 23:05:47 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BanApp::Application.config.secret_key_base = 'b208f67e129e763426eb1dfccb25eea08691d5959ed5588da7c1abd4b6790a61c8aef948bb3e8fc5c4f6f6f492df4245ec245a5c7e653101c1f41abf42fa0b73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BanApp::Application.config.session_store :cookie_store, key: '_ban_app_session'
